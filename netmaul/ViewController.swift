1cc8295289c3636a4a102fef81457258ab3e85e1
최재연 <choejaeyeon@choejaeyeonui-MacBook.local>
2016-12-24 23:40:30 +0900

    Initial Commit

---
+++ netmaul/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netmaul
+//
+//  Created by 최재연 on 2016. 12. 24..
+//  Copyright © 2016년 최재연new. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ netmaulTests/netmaulTests.swift
@@ -0,0 +1,36 @@
+//
+//  netmaulTests.swift
+//  netmaulTests
+//
+//  Created by 최재연 on 2016. 12. 24..
+//  Copyright © 2016년 최재연new. All rights reserved.
+//
+
+import XCTest
+@testable import netmaul
+
+class netmaulTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
